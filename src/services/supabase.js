6ee2e803e25bc5830d71d18838f5da0a5bf08bc0
mohamed <mohamed97@gmail.com>
2023-09-06 00:20:06 +0300

    upload new cabin and images to supabase & handling form error

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vutxehljdkbfutgbtwyf.supabase.co";
+export const supabaseUrl = "https://vutxehljdkbfutgbtwyf.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1dHhlaGxqZGtiZnV0Z2J0d3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MzI5ODEsImV4cCI6MjAwOTQwODk4MX0.EjIsiNL9GS6uBhZUHvW1zTj9v9VEM-o-_cAVe_OcPRs";
 const supabase = createClient(supabaseUrl, supabaseKey);
