b2efc879b31b24aa54e83492d1a3d5e6096febca
mohamed <mohamed97@gmail.com>
2023-09-11 23:20:27 +0300

    add Dashboard features

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
 import Sidebar from "./Sidebar";
-import { styled } from "styled-components";
+import Header from "./Header";
+import styled from "styled-components";
 
 const StyledAppLayout = styled.div`
   display: grid;
@@ -13,7 +13,7 @@ const StyledAppLayout = styled.div`
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
-  overflow: scroll;
+  overflow: auto;
 `;
 
 const Container = styled.div`
